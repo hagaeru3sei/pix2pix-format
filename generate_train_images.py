564927277025a9af74fa786386048e92f8a18f78
Nobuaki Mochizuki <hagaeru3sei@gmail.com>
2018-07-03 17:54:23 +0900

    Add resize mothod

--- generate_train_images.py
+++ generate_train_images.py
@@ -5,23 +5,26 @@ from glob import glob
 
 
 image_origin_dir = './images/origin'
+image_resized_dir = './images/resized'
 image_mono_dir = './images/mono'
 image_train_dir = './images/pix2pix_format'
 
 
-def _resize(path) -> None:
-    pass
+def _resize(filename) -> None:
+    origin = cv2.imread('%s/%s' % (image_origin_dir, filename,))
+    resized = cv2.resize(origin, None, fx=0.2, fy=0.2)
+    cv2.imwrite('%s/%s' % (image_resized_dir, filename,), resized)
 
 
 def _merge_image(filename) -> None:
     """
     A is Grayscale
-    B is Original
+    B is Colorful
     +---+   +---+    +------+
     | A | + | B | -> | A  B |
     +---+   +---+    +------+
     """
-    origin = cv2.imread('%s/%s' % (image_origin_dir, filename,))
+    origin = cv2.imread('%s/%s' % (image_resized_dir, filename,))
     mono = cv2.imread('%s/%s' % (image_mono_dir, filename,))
     new_canvas = cv2.hconcat([mono, origin])
     cv2.imwrite('%s/%s' % (image_train_dir, filename,), new_canvas)
@@ -30,10 +33,11 @@ def _merge_image(filename) -> None:
 def main():
     files = glob('%s/*.jpg' % (image_origin_dir,))
     for image_path in files:
+        # origin to resized
         _resize(image_path)
         filename = image_path.split('/')[-1]
         # Create Grayscale Image
-        img = cv2.imread(image_path)
+        img = cv2.imread('%s/%s' % (image_resized_dir, filename,))
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         cv2.imwrite('%s/%s' % (image_mono_dir, filename,), gray)
         # 
